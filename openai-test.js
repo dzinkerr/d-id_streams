d15c8902592cbafca464bb3463680a5449831811
dzinkerr <42792725+dzinkerr@users.noreply.github.com>
2023-07-05 12:06:32 -0600

    FuncionaOpenAIAPIPrimeraVez

--- openai-test.js
+++ openai-test.js
@@ -1,5 +1,5 @@
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
-const endpoint = "https://prft-openai.openai.azure.com/";
+const endpoint = 'https://prft-openai.openai.azure.com';
 const azureApiKey = "591ef217d0814ec4b104d015f4cf386c" ;
 
 const prompt = ["When was Microsoft founded?"];
@@ -8,7 +8,7 @@ async function main() {
   console.log("== Get completions Sample ==");
 
   const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-  const deploymentId = "text-davinci-003";
+  const deploymentId = "test-deployment-davinci";
   const result = await client.getCompletions(deploymentId, prompt);
 
   for (const choice of result.choices) {
